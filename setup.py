db67be98f7d85ec8c696b44063342eca64f55e31
st81 <58893365+st81@users.noreply.github.com>
2023-02-23 13:22:13 +0900

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+
+setuptools.setup(
+    name="my_stable_baselines3",
+    version="0.0.1",
+    author="cm-taro",
+    author_email="cmtaro@example.com",
+    license='MIT',
+    description="You can receive AWS Service Name.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/st81/tutorial-pip-github-repo",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    packages=setuptools.find_packages(),
+    python_requires=">=3.7",
+)
